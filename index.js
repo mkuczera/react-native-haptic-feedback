f3973bd9f219b778cf892c4bcc67ae51ae5f0b2e
Alan Kenyon <alan@shipt.com>
2019-04-04 10:23:57 -0500

    reorganized again

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ import { NativeModules, Platform } from 'react-native';
 
 class RNReactNativeHapticFeedback {
     static trigger = (type = 'selection', options = {}) => {
-        // if options is a bool we're using a v1.6 api. This only effects iOS
+        // if options is a bool we're using a v1.6 api.
         if (typeof options === 'boolean' && Platform === 'ios') {
-            return trigger(type, options)
+            triggerHaptic(type, options)
+        } else if (typeof options === 'boolean') {
+            triggerHaptic(type, {})
+        } else {
+            const mergedOptions = {
+                enableVibrateFallback: false,
+                ignoreAndroidSystemSettings: false,
+                ...options
+            }
+    
+            triggerHaptic(type, mergedOptions)
         }
-
-        const mergedOptions = {
-            enableVibrateFallback: false,
-            ignoreAndroidSystemSettings: false,
-            ...options
-        }
-
-        trigger(type, mergedOptions)
     }
 }
 
-const trigger(type, options) {
+const triggerHaptic(type, options) {
     try {
         NativeModules.RNReactNativeHapticFeedback.trigger(type, options);
     } catch (err) {

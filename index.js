cb0a74ff14f54507c02fe8ea4be36515974c749d
Alan Kenyon <alan@shipt.com>
2019-04-04 10:19:20 -0500

    cleaning up

--- index.js
+++ index.js
@@ -2,21 +2,26 @@ import { NativeModules, Platform } from 'react-native';
 
 class RNReactNativeHapticFeedback {
     static trigger = (type = 'selection', options = {}) => {
+        // if options is a bool we're using a v1.6 api. This only effects iOS
+        if (typeof options === 'boolean' && Platform === 'ios') {
+            return trigger(type, options)
+        }
+
         const mergedOptions = {
             enableVibrateFallback: false,
             ignoreAndroidSystemSettings: false,
             ...options
         }
 
-        try {
-            if (typeof options === 'boolean' && Platform === 'ios') {
-                NativeModules.RNReactNativeHapticFeedback.trigger(type, options);
-            } else {
-                NativeModules.RNReactNativeHapticFeedback.trigger(type, mergedOptions);
-            }
-        } catch (err) {
-            console.warn('RNReactNativeHapticFeedback is not available');
-        }
+        trigger(type, mergedOptions)
+    }
+}
+
+const trigger(type, options) {
+    try {
+        NativeModules.RNReactNativeHapticFeedback.trigger(type, options);
+    } catch (err) {
+        console.warn('RNReactNativeHapticFeedback is not available');
     }
 }
 

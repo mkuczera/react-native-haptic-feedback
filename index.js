5f2ab2cad4100e5b69f24c16471c409d23cd01f7
Alan Kenyon <alan@shipt.com>
2019-04-09 13:42:54 -0700

    use default options for old api with Android

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 import { NativeModules, Platform } from 'react-native';
 
+const defaultOptions = {
+    enableVibrateFallback: false,
+    ignoreAndroidSystemSettings: false,
+}
+
 class RNReactNativeHapticFeedback {
     static trigger = (type = 'selection', options = {}) => {
         // if options is a bool we're using a v1.6 api.
         if (typeof options === 'boolean' && Platform === 'ios') {
             triggerHaptic(type, options)
         } else if (typeof options === 'boolean') {
-            triggerHaptic(type, {})
-        } else {
-            const mergedOptions = {
-                enableVibrateFallback: false,
-                ignoreAndroidSystemSettings: false,
-                ...options
-            }
-    
-            triggerHaptic(type, mergedOptions)
+            triggerHaptic(type, defaultOptions)
+        } else {   
+            triggerHaptic(type, { ...defaultOptions, ...options })
         }
     }
 }

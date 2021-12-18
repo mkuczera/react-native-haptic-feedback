47a92d68b68e6229ddb5459c24b3a3dfe5c8c8df
abing <abing@gcores.com>
2021-12-18 13:43:25 +0800

    fix: change header import style to work with latest Expo SDK

--- ios/RNReactNativeHapticFeedback.h
+++ ios/RNReactNativeHapticFeedback.h
@@ -1,11 +1,7 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
-#endif
 
 @interface RNReactNativeHapticFeedback : NSObject <RCTBridgeModule>
 

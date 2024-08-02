1a6536f7a28ba1fe1d18ce66e29c4f6db3b11ed8
Michael Kuczera <m.kuczera@gmail.com>
2024-08-02 13:47:47 +0200

    Fix linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,16 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  { ignores: ["build/*", "node_modules/*", "example/*"] },
+  {
+    ignores: [
+      "build/*",
+      "node_modules/*",
+      "example/*",
+      "setupTests.js",
+      "babel.config.js",
+      "jest.config.js",
+    ],
+  },
   {
     rules: {
       "@typescript-eslint/no-require-imports": "off",
